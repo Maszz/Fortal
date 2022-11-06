9e9081da70286ae058cb6f09e01b405c157c6167
Masz <mawin38408@gmail.com>
2022-11-07 04:24:56 +0700

    merge

---
+++ app/screens/SettingScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'native-base';
+import {FunctionComponent} from 'react';
+import {SettingScreenProps} from '../types';
+const SettingScreen: FunctionComponent<SettingScreenProps> = () => {
+  return (
+    <View>
+      <Text>SettingScreen</Text>
+    </View>
+  );
+};
+export default SettingScreen;
