f2dbcadf496978c521ce8cc3c7f4faad47d13cd1
Masz <mawin38408@gmail.com>
2022-11-14 21:52:24 +0700

    add animate

--- app/screens/EventNote.tsx
+++ app/screens/EventNote.tsx
@@ -10,10 +10,19 @@ import {
   Spacer,
 } from 'native-base';
 import {EventNoteScreenProps} from '../types';
-import {FunctionComponent} from 'react';
-import {TouchableOpacity} from 'react-native';
+import {FunctionComponent, useEffect, useState, useRef} from 'react';
+import {
+  TouchableOpacity,
+  LayoutAnimation,
+  Animated,
+  Easing,
+} from 'react-native';
 import {Divider, Input} from 'native-base';
-const EventNote: FunctionComponent<EventNoteScreenProps> = () => {
+const EventNote: FunctionComponent<EventNoteScreenProps> = ({navigation}) => {
+  const [pinPostProps, setPinPostProps] = useState<{
+    height: string;
+  }>({height: 'auto'});
+  const fadeAnim = useRef(new Animated.Value(1)).current;
   return (
     <View flex={1} backgroundColor={'white'}>
       {/* head */}
@@ -29,7 +38,7 @@ const EventNote: FunctionComponent<EventNoteScreenProps> = () => {
           justifyContent={'space-between'}>
           <TouchableOpacity
             onPress={() => {
-              // navigation.goBack();
+              navigation.goBack();
             }}>
             <Image
               width={30}
@@ -51,126 +60,188 @@ const EventNote: FunctionComponent<EventNoteScreenProps> = () => {
             <Text fontSize={16} color={'#232259'} fontWeight={'normal'}>
               ประกาศ
             </Text>
-            <Image
-              source={require('../assets/uparrow_icon.png')}
-              borderRadius={5}
-            />
-          </HStack>
-
-          <HStack>
-            <Image
-              source={require('../assets/groupAlert_icon.png')}
-              // borderWidth={3}
-              borderColor={'#8172F7'}
-            />
-            <VStack width={'100%'} marginX={5}>
-              <Text fontSize={16} color={'#232259'} fontWeight={'bold'}>
-                Header name
-              </Text>
-              <Text fontSize={14} color={'#232259'} fontWeight={'normal'}>
-                date : time
-              </Text>
-              <Box
-                justifyItems={'center'}
-                marginTop={2}
-                borderWidth={10}
-                borderColor={'#8C84D4'}
-                borderRadius={20}
-                width={'75%'}>
-                <Text
-                  textAlign={'left'}
-                  paddingX={1}
-                  color={'white'}
-                  fontSize={16}
-                  fontWeight={'normal'}
-                  style={{backgroundColor: '#8C84D4'}}>
-                  message Taken from the Latin words "dolorem ipsum", which
-                  translates to "pain itself", Lorem Ipsum text saw a revival in
-                  the mid-20th century as
-                </Text>
-              </Box>
-            </VStack>
-          </HStack>
-        </Box>
-        <Divider my={5} opacity={0} bg="black" />
-        <Text fontSize={16} color={'#232259'} fontWeight={'normal'}>
-          Post
-        </Text>
-        <Divider my={2} />
-        <ScrollView variant={'vertical'} height={'100%'}>
-          {/* this Box is triger component of member group post */}
-          <Box
-            mt={5}
-            borderBottomWidth={1}
-            paddingBottom={5}
-            borderBottomColor={'#8B9093'}>
-            <HStack>
+            <TouchableOpacity
+              onPress={() => {
+                // asd
+                LayoutAnimation.configureNext({
+                  duration: 300,
+                  create: {
+                    springDamping: 0.7,
+                    type: 'spring',
+                    property: 'scaleXY',
+                  },
+                  update: {
+                    springDamping: 0.7,
+                    type: 'spring',
+                    property: 'scaleXY',
+                  },
+                  delete: {
+                    springDamping: 0.7,
+                    type: 'spring',
+                    property: 'scaleXY',
+                  },
+                });
+                if (pinPostProps.height === 'auto') {
+                  setPinPostProps({height: '0%'});
+                  Animated.timing(fadeAnim, {
+                    toValue: 0,
+                    duration: 150,
+                    easing: Easing.bounce,
+                    useNativeDriver: true,
+                  }).start();
+                } else {
+                  setPinPostProps({height: 'auto'});
+                  Animated.timing(fadeAnim, {
+                    toValue: 1,
+                    duration: 150,
+                    useNativeDriver: true,
+                  }).start();
+                }
+              }}>
               <Image
-                source={require('../assets/profileGroupPost_icon.png')}
+                source={require('../assets/uparrow_icon.png')}
+                style={{
+                  transform: [
+                    {
+                      rotate:
+                        pinPostProps.height === 'auto' ? '180deg' : '0deg',
+                    },
+                  ],
+                }}
+                alt={'up arrow'}
+                borderRadius={5}
+              />
+            </TouchableOpacity>
+          </HStack>
+          <Animated.View style={{opacity: fadeAnim}}>
+            <HStack h={pinPostProps.height}>
+              <Image
+                source={require('../assets/groupAlert_icon.png')}
                 // borderWidth={3}
+                alt={'group alert'}
                 borderColor={'#8172F7'}
               />
               <VStack width={'100%'} marginX={5}>
                 <Text fontSize={16} color={'#232259'} fontWeight={'bold'}>
-                  Poster name
+                  Header name
                 </Text>
                 <Text fontSize={14} color={'#232259'} fontWeight={'normal'}>
-                  13:15
+                  date : time
                 </Text>
-                <Text fontSize={14} color={'#232259'} fontWeight={'normal'}>
-                  Tue, 10 Aug
-                </Text>
+                <Box
+                  justifyItems={'center'}
+                  marginTop={2}
+                  borderWidth={10}
+                  borderColor={'#8C84D4'}
+                  borderRadius={20}
+                  width={'75%'}>
+                  <Text
+                    textAlign={'left'}
+                    paddingX={1}
+                    color={'white'}
+                    fontSize={16}
+                    fontWeight={'normal'}
+                    style={{backgroundColor: '#8C84D4'}}>
+                    message Taken from the Latin words "dolorem ipsum", which
+                    translates to "pain itself", Lorem Ipsum text saw a revival
+                    in the mid-20th century as
+                  </Text>
+                </Box>
               </VStack>
             </HStack>
-            <Divider my={2} opacity={0} />
-            <Text
-              paddingX={'5%'}
-              fontSize={16}
-              fontWeight={'normal'}
-              color={'#232259'}>
-              Taken from the Latin words "dolorem ipsum", which translates to
-              "pain itself", Lorem Ipsum text saw a revival in the mid-20th
-              century as
-            </Text>
-            <Divider my={3} opacity={0} />
-            <HStack
-              // marginTop={3}
-              alignSelf={'center'}
-              width={'90%'}
-              justifyItems={'center'}
-              alignItems={'center'}
-              paddingX={'4%'}
-              backgroundColor={'white'}
-              borderRadius={'full'}
-              borderColor={'#8172F7'}
-              borderWidth={2}>
-              <Image
-                source={require('../assets/smile_icon.png')}
-                alt={'smile'}
-              />
-              <Input
-                width={'80%'}
-                marginLeft={2}
-                placeholder="Text hear!"
-                variant={'unstyle'}
-                // value={userInput}
-                onChangeText={text => setUserInput(text)}
-              />
-              <TouchableOpacity
-                onPress={() => {
-                  //
-                }}>
-                <Image
-                  source={require('../assets/send_icon.png')}
-                  alt={'send'}
-                />
-              </TouchableOpacity>
-            </HStack>
-          </Box>
-        </ScrollView>
+          </Animated.View>
+        </Box>
+        <Divider my={5} opacity={0} bg="black" />
+        <Box>
+          <Text fontSize={16} color={'#232259'} fontWeight={'normal'}>
+            Post
+          </Text>
+          <Divider my={2} />
+          <ScrollView variant={'vertical'} height={'100%'}>
+            {/* this Box is triger component of member group post */}
+            <EventPost />
+          </ScrollView>
+        </Box>
       </VStack>
     </View>
   );
 };
 
 export default EventNote;
+export interface EventPostProps {
+  name: string;
+  date: string;
+  time: string;
+  message: string;
+}
+const EventPost: FunctionComponent<EventPostProps> = ({
+  name,
+  time,
+  date,
+  message,
+}) => {
+  return (
+    <Box
+      mt={5}
+      borderBottomWidth={1}
+      paddingBottom={5}
+      borderBottomColor={'#8B9093'}>
+      <HStack>
+        <Image
+          source={require('../assets/profileGroupPost_icon.png')}
+          // borderWidth={3}
+          borderColor={'#8172F7'}
+        />
+        <VStack width={'100%'} marginX={5}>
+          <Text fontSize={16} color={'#232259'} fontWeight={'bold'}>
+            Poster name{name}
+          </Text>
+          <Text fontSize={14} color={'#232259'} fontWeight={'normal'}>
+            13:15{time}
+          </Text>
+          <Text fontSize={14} color={'#232259'} fontWeight={'normal'}>
+            Tue, 10 Aug{date}
+          </Text>
+        </VStack>
+      </HStack>
+      <Divider my={2} opacity={0} />
+      <Text
+        paddingX={'5%'}
+        fontSize={16}
+        fontWeight={'normal'}
+        color={'#232259'}>
+        Taken from the Latin words "dolorem ipsum", which translates to "pain
+        itself", Lorem Ipsum text saw a revival in the mid-20th century as
+        {message}
+      </Text>
+      <Divider my={3} opacity={0} />
+      <HStack
+        // marginTop={3}
+        alignSelf={'center'}
+        width={'90%'}
+        justifyItems={'center'}
+        alignItems={'center'}
+        paddingX={'4%'}
+        backgroundColor={'white'}
+        borderRadius={'full'}
+        borderColor={'#8172F7'}
+        borderWidth={2}>
+        <Image source={require('../assets/smile_icon.png')} alt={'smile'} />
+        <Input
+          width={'80%'}
+          marginLeft={2}
+          placeholder="Text hear!"
+          variant={'unstyle'}
+          // value={userInput}
+          onChangeText={text => setUserInput(text)}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            //
+          }}>
+          <Image source={require('../assets/send_icon.png')} alt={'send'} />
+        </TouchableOpacity>
+      </HStack>
+    </Box>
+  );
+};
